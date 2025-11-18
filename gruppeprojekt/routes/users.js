13852c09d47038a4425d3bf408ebe9c97ca566d9
Emil-indiantechsupport <empa24af@student.cbs.dk>
2025-11-18 08:53:38 +0100

    route

--- gruppeprojekt/routes/users.js
+++ gruppeprojekt/routes/users.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
+/*
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-
+*/
+router.get('/oplevelse.html', function (req, res) {
+    res.render('public/oplevelse.html');
+});
 
 module.exports = router;
