d24628c476051c065433f1795f08a8ff5326bc86
Meda Norkute <meduoliukas12@gmail.com>
2025-11-24 22:41:38 +0100

    rate limiter er done

---
+++ gruppeprojekt/middlewares/rateLimiter.js
@@ -0,0 +1,14 @@
+
+// Her bygger vi en rate limiter som kan burgues på vores webhooks i twilio 
+const rateLimit = require('express-rate-limit');
+
+const twilioRateLimiter = rateLimit({
+    windowMs: 10 * 1000, // 10 sekunder hvor en bruger kan sende requests
+    max: 3, // maks 3 requests pr 10 sekunder
+    message: 'For mange requests fra denne IP'
+});
+
+
+module.exports = {
+    twilioRateLimiter
+};
--- gruppeprojekt/routes/kollabRoutes.js
+++ gruppeprojekt/routes/kollabRoutes.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 const smsController = require('../controllers/smsController');
 const webhookController = require('../controllers/webhookController');
-
+const { twilioRateLimiter } = require('../middlewares/rateLimiter'); // importerer rate limiterne
 // POST /api/kollab/send-sms - Send SMS til vært
 router.post('/send-sms', smsController.sendKollabSMS);
 
 // POST /api/kollab/webhook - Twilio webhook for indkommende SMS (når vært B svarer)
-router.post('/webhook', (req, res, next) => {
-    
-    next();
-}, webhookController.twilioWebhook);
+// Rate limiteren tjekker først, derefter håndteres webhook
+router.post('/webhook', twilioRateLimiter, webhookController.twilioWebhook);
 
 module.exports = router;
